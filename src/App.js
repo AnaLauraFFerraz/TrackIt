2d9b7ec510ca3be158f7117808f1cd2c78cd7d3a
AnaLauraFFerraz <analaurafferraz@gmail.com>
2022-12-18 03:17:54 -0300

    components created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Habits from './pages/Habits'
+import Today from './pages/Today'
+import History from './pages/History'
+
 function App() {
+
   return (
-    <div></div>
+    <UserContext.Provider value={{ user, setUser, percentage, setPercentage }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastro" element={<SignUp />} />
+          <Route path="/habitos" element={<Habits />} />
+          <Route path="/hoje" element={<Today />} />
+          <Route path="/historico" element={<History />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
